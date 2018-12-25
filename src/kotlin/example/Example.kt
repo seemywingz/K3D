29f10f0701e5ac2fb0e89902e2c47ddab6a69fe8
Kevin Jayne <kevin.jayne@icloud.com>
2018-12-25 15:25:23 -0500

    update example display func

--- src/kotlin/example/Example.kt
+++ src/kotlin/example/Example.kt
@@ -10,26 +10,27 @@ import platform.OpenGLCommon.*
 
 private var rotation: GLfloat = 0.0f
 private val rotationSpeed: GLfloat = 0.2f
-//var displayFunction: () -> Unit { -> println("Foo") }
+
+fun displayFunction(){
+    // Define a viewing transformation
+    gluLookAt(4.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+
+    // Push and pop the current matrix stack.
+    // This causes the translations and rotations of this matrix not to influence others.
+    glPushMatrix()
+    glColor3f(1.0f, 0.0f, 0.0f)
+    glTranslatef(0.0f, 0.0f, 0.0f)
+    glRotatef(rotation, 0.0f, 1.0f, 0.0f)
+    glRotatef(90.0f, 0.0f, 1.0f, 0.0f)
+
+    // Draw the teapot
+    glutSolidTeapot(1.0)
+    glutSolidSphere(1.0, 70, 70)
+    glPopMatrix()
+
+    rotation += rotationSpeed
+}
 
 fun main(){
-    K3D.initialize() {
-        // Define a viewing transformation
-         gluLookAt(4.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
-
-        // Push and pop the current matrix stack.
-        // This causes the translations and rotations of this matrix not to influence others.
-        glPushMatrix()
-        glColor3f(1.0f, 0.0f, 0.0f)
-        glTranslatef(0.0f, 0.0f, 0.0f)
-        glRotatef(rotation, 0.0f, 1.0f, 0.0f)
-        glRotatef(90.0f, 0.0f, 1.0f, 0.0f)
-
-        // Draw the teapot
-        glutSolidTeapot(1.0)
-        glutSolidSphere(1.0, 70, 70)
-        glPopMatrix()
-
-        rotation += rotationSpeed
-    }
+    K3D.initialize() { displayFunction() }
 }
